761f15944bf4f7f900db22b6c3e0e4c779089d45
Md Mahbub Alom <mdmahbuba034@gmail.com>
2024-06-14 02:22:20 +0600

    my class added successfully

--- index.js
+++ index.js
@@ -80,6 +80,16 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/getClass", async (req, res) => {
+      const user = req?.query?.email;
+      let query = {};
+      if (user) {
+        query = { instructorEmail: user };
+      }
+      const result = await classCollection.find(query).toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
